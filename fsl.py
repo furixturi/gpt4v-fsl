87d485477c5fcceaa5a7a53faa922d42fc511df6
Xiaoli Alex Shen <alabebop@gmail.com>
2024-04-16 20:10:03 +0900

    move test to a separate file

--- fsl.py
+++ fsl.py
@@ -6,7 +6,6 @@ import os, base64, json, logging, time, asyncio
 # from openai import AzureOpenAI
 from openai import AsyncAzureOpenAI
 
-
 class FewShotLearning:
     def __init__(
         self,
@@ -196,45 +195,4 @@ class FewShotLearning:
         task_few = asyncio.create_task(self.few_shot())
         responses = await asyncio.gather(task_0, task_1, task_few)
         return responses
-
-# Test it
-if __name__ == "__main__":
-    # variables
-    question = "Extract parking fee information from the picture and give me a list."
-    examples = [
-        {
-            "image_path": "./sample_images/parking-fee-sample-1.png",
-            "output": "- 8:00-22:00 220 yen / 40 min\n- 22:00-8:00 110 yen / 60 min\n- max: 1100 yen",
-        },
-        {
-            "image_path": "./sample_images/parking-fee-sample-2.png",
-            "output": "- 8:00-22:00 330 yen / 60 min\n- 22:00-8:00 110 yen / 60 min\n- max: 770 yen / 24 hours",
-        },
-        {
-            "image_path": "./sample_images/parking-fee-sample-3.png",
-            "output": "- 8:00-20:00 440 yen / 12 min\n- 20:00-8:00 110 yen / 60 min\n- max: 2800 yen / 5 hours",
-        },
-        {
-            "image_path": "./sample_images/parking-fee-sample-4.png",
-            "output": "-110 yen / 20 min\n- 8:00-19:00 max 880 yen\n- 19:00-8:00 max 440 yen",
-        },
-    ]
-    test_image_path = "./sample_images/parking-fee-test-1.png"
-
-    fsl = FewShotLearning(
-        examples=examples, question=question, test_image_url=test_image_path
-    )
     
-    ## Sequential
-    # response = asyncio.run(fsl.zero_shot())
-    # print(response.choices[0].message.content)
-    # response_1 = asyncio.run(fsl.one_shot())
-    # print(response_1.choices[0].message.content)
-    # response_2 = asyncio.run(fsl.few_shot())
-    # print(response_2.choices[0].message.content)
-    
-    # Concurrent
-    responses = asyncio.run(fsl.run_all_shots())
-    for idx, res in enumerate(responses):
-        print(f'==== {idx} ====')
-        print(res.choices[0].message.content)
